5be58630f5ffcd7db1aee485caf4cb6b671d328f
Julien L <Julien L>
2016-08-18 18:22:20 -0400

    app julien

---
+++ appJulien/app/pages/home/home.component.ts
@@ -0,0 +1,22 @@
+import {Component} from "@angular/core";
+
+@Component({
+    selector: "my-app",
+    templateUrl: "./pages/home/home.component.html",
+    styleUrls: ["./pages/home/home.css"]
+})
+export class HomeComponent {
+    public counter: number = 16;
+
+    public get message(): string {
+        if (this.counter > 0) {
+            return this.counter + " taps left";
+        } else {
+            return "!!Hoorraaay! \nYou are ready to start building!";
+        }
+    }
+    
+    public onTap() {
+        this.counter--;
+    }
+}
