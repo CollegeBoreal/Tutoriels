d2456b1fb7955191517863931b3074276c8626cb
Brice <62551735+b300098957@users.noreply.github.com>
2020-05-23 15:00:08 -0400

    Create main.tf

---
+++ 2.Virtualisation/5.IaC/2.Terraform/GCP/tfinfra/instance/main.tf
@@ -0,0 +1,26 @@
+variable "instance_name" {}
+variable "instance_zone" {}
+variable "instance_type" {
+  default = "n1-standard-1"
+  }
+variable "instance_subnetwork" {
+}
+
+resource "google_compute_instance" "vm_instance" {
+  name         = "${var.instance_name}"
+  project      = "qwiklabs-gcp-04-58acf6ab89fb"
+  subnetwork_project = "qwiklabs-gcp-04-58acf6ab89fb"
+  zone         = "${var.instance_zone}"
+  machine_type = "${var.instance_type}"
+  boot_disk {
+    initialize_params {
+      image = "debian-cloud/debian-9"
+      }
+  }
+  network_interface {
+    subnetwork = "${var.instance_subnetwork}"
+    access_config {
+      # Allocate a one-to-one NAT IP to the instance
+    }
+  }
+}
