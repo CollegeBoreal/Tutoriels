0243439c92c6bf8072ca07f3be5c71fd77b2bf04
b300098957 <300098957@collegeboreal.ca>
2020-05-24 15:41:50 -0400

    adjusting variables

--- 2.Virtualisation/4.Cloud/2.Public/3.OCI/tf/provider.tf
+++ 2.Virtualisation/4.Cloud/2.Public/3.OCI/tf/provider.tf
@@ -5,7 +5,7 @@ variable "region" {}
 
 # Configure the Oracle Cloud Infrastructure provider with an API Key
 provider "oci" {
-  tenancy_ocid = "${var.tenancy_ocid}"
-  user_ocid = "${var.user_ocid}"
-  region = "${var.region}"
+  tenancy_ocid = var.tenancy_ocid
+  user_ocid = var.user_ocid
+  region = var.region
 }
